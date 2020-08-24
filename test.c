d4024934a40262fee616ab5d289fd4f5fb4cd034
amp5646@yahoo.com <amp5646@yahoo.com>
2020-08-24 12:02:00 -0400

    Added test file for noobs

---
+++ test.c
@@ -0,0 +1,7 @@
+//Try out opening this folder and then typing g++ test.c -o test.exe. Then type test.exe in the terminal to run the command
+
+#include <stdio.h>
+int main()
+{
+    printf("Hello world");
+}

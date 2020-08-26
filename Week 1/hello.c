9471c4020cc80f2ee01ae2a16d708918f5a5482f
amp5646@yahoo.com <amp5646@yahoo.com>
2020-08-26 12:26:49 -0400

    Week 1 Wednesday

--- Week 1/hello.c
+++ Week 1/hello.c
@@ -1,7 +1,11 @@
 //Try out opening this folder and then typing g++ test.c -o test.exe. Then type test.exe in the terminal to run the command
 
 #include <stdio.h>
-int main()
+#include <stdlib.h>
+
+int main(void)
 {
     printf("Hello world");
+
+    return 0;
 }

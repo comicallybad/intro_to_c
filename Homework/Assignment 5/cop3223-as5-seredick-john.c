b2ccefdb1a99863a60cb8a1ab8d0a68c3b3eb7c4
comicallybad <amp5646@yahoo.com>
2020-11-27 23:58:49 -0500

    Assignment 5

--- Homework/Assignment 5/cop3223-as5-seredick-john.c
+++ Homework/Assignment 5/cop3223-as5-seredick-john.c
@@ -2,33 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-void remove_crlf(char *s)
-{
-    char *t = s + strlen(s); // t begins at the null sentinel at the end of s.
-    t--;
-    /* t is now at the last character of s - unless s didn't contain any characters, in        
-     which case, t is now *BEFORE* s.  We have to keep checking for that. */
-    /* We repeat until EITHER t slides to the left of s, OR we find a character that is        
-     not a line feed (\n) or a carriage return (\r). */
-    while ((t >= s) && (*t == '\n' || *t == '\r'))
-    {
-        *t = '\0'; // Clobber the character t is pointing at.
-        t--;       // Decrement t.
-    }
-}
-
 struct line_struct
 {
     float m, b;
     int count;
-    double x, y;
+    double *x, *y;
 };
 
 typedef struct line_struct line;
 
-line *make_line(float m, float b, int count, double x, double y)
+line *make_line(float m, float b, int count, double *x, double *y)
 {
-    line *l = malloc(sizeof(line));
+    line *l = malloc(sizeof(line) + sizeof(x) + sizeof(y));
     l->m = m;
     l->b = b;
     l->count = count;
@@ -38,22 +23,23 @@ line *make_line(float m, float b, int count, double x, double y)
     return l;
 }
 
-line *read_line(FILE *ifp)
+void write_line(FILE *ofp, line *l)
 {
-    line *l = malloc(sizeof(line));
+    fprintf(ofp, "m: %f\n", l->m);
+    fprintf(ofp, "b: %f\n", l->b);
+    fprintf(ofp, "count: %d\n", l->count);
 
-    fread(l, sizeof(line), 1, ifp);
-    return l;
+    for (int i = 0; i < l->count; i++)
+    {
+        fprintf(ofp, "X: %3.3lf  Y:%3.3lf\n", l->x[i], l->y[i]);
+    }
 }
 
-void write_small_monster(FILE *ofp, line *l)
-{
-    fwrite(l, sizeof(line), 1, ofp);
-}
-
-void dispose_line(line *l)
+void dispose_line(line *l, double *x, double *y)
 {
     free(l);
+    free(x);
+    free(y);
 }
 
 void print_line(line *l)
@@ -61,28 +47,75 @@ void print_line(line *l)
     printf("m: %f\n", l->m);
     printf("b: %f\n", l->b);
     printf("count: %d\n", l->count);
-    printf("x: %lf\n", l->x);
-    printf("y: %lf\n", l->y);
+
+    for (int i = 0; i < l->count; i++)
+    {
+        printf("X: %3.3lf  Y:%3.3lf\n", l->x[i], l->y[i]);
+    }
+}
+
+double find_y(float m, double x, float b)
+{
+    return (m * x) + b;
 }
 
 int main(void)
 {
+    //Declare variables
     float m, b;
     int count;
-    double x, y;
+    double *x, *y;
     char buf[256];
 
+    //Open input file
     FILE *ifp;
-
     ifp = fopen("as5input.txt", "r");
 
+    //Get m and b from same line and print it
     fgets(buf, 255, ifp);
     sscanf(buf, "%f %f", &m, &b);
     printf("m:%f b:%f\n", m, b);
 
+    //Get count from line and print it
     fgets(buf, 255, ifp);
     sscanf(buf, "%d", &count);
     printf("count: %d\n", count);
+
+    //Allocate enough memory for x and y arrays
+    x = malloc(sizeof(double) * count);
+    y = malloc(sizeof(double) * count);
+
+    //Get line of x values
+    fgets(buf, 255, ifp);
+
+    //Get every double (don't have to consider count, that is why we allocated count * sizeof(double))
+    int i = 0;
+    char *n = strtok(buf, " ");
+    do
+    {
+        x[i++] = atof(n);
+    } while (n = strtok(NULL, " "));
+
+    //Close file as we are done reading
+    fclose(ifp);
+
+    //Find y values for each x value
+    for (int i = 0; i < count; i++)
+    {
+        y[i] = find_y(m, x[i], b);
+    }
+
+    //Create our new line structure and print it out
+    line *l = make_line(m, b, count, x, y);
+    print_line(l);
+
+    //Open output file and write line to output file
+    FILE *ofp;
+    ofp = fopen("as5output-seredick-john.txt", "w");
+    write_line(ofp, l);
+
+    //Free up the memory we used for line, x and y
+    dispose_line(l, x, y);
 
     return 0;
 }
